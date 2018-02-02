1c1cbe4e46effe5368f4b641975573e7d508b724
JHades <jhades.dev@gmail.com>
2018-02-02 11:21:55 +0100

    Angular Universal Course

--- server/db-data.ts
+++ server/db-data.ts
@@ -74,214 +74,214 @@ export const COURSES: any = [
 export const LESSONS = [
 
      {
-        id: 1,
+        id: "1",
         "description": "Angular Tutorial For Beginners - Build Your First App - Hello World Step By Step",
         "duration": "4:17",
         "seqNo": 1,
-        courseId: 1
+        courseId: "1"
     },
      {
-        id: 2,
+        id: "2",
         "description": "Building Your First  Component - Component Composition",
         "duration": "2:07",
         "seqNo": 2,
-        courseId: 1
+        courseId: "1"
     },
     {
-        id: 3,
+        id: "3",
         "description": "Component @Input - How To Pass Input Data To an  Component",
         "duration": "2:33",
         "seqNo": 3,
-        courseId: 1
+        courseId: "1"
     },
     {
-        id: 4,
+        id: "4",
         "description": " Component Events - Using @Output to create custom events",
         "duration": "4:44",
         "seqNo": 4,
-        courseId: 1
+        courseId: "1"
     },
     {
-        id: 5,
+        id: "5",
         "description": " Component Templates - Inline Vs External",
         "duration": "2:55",
         "seqNo": 5,
-        courseId: 1
+        courseId: "1"
     },
      {
-        id: 6,
+        id: "6",
         "description": "Styling  Components - Learn About Component Style Isolation",
         "duration": "3:27",
         "seqNo": 6,
-        courseId: 1
+        courseId: "1"
     },
     {
-        id: 7,
+        id: "7",
         "description": " Component Interaction - Extended Components Example",
         "duration": "9:22",
         "seqNo": 7,
-        courseId: 1
+        courseId: "1"
     },
     {
-        id: 8,
+        id: "8",
         "description": " Components Tutorial For Beginners - Components Exercise !",
         "duration": "1:26",
         "seqNo": 8,
-        courseId: 1
+        courseId: "1"
     },
      {
-        id: 9,
+        id: "9",
         "description": " Components Tutorial For Beginners - Components Exercise Solution Inside",
         "duration": "2:08",
         "seqNo": 9,
-        courseId: 1
+        courseId: "1"
     },
     {
-        id: 10,
+        id: "10",
         "description": " Directives - Inputs, Output Event Emitters and How To Export Template References",
         "duration": "4:01",
         "seqNo": 10,
-        courseId: 1
+        courseId: "1"
     },
     // Security Course
     {
-        id: 11,
+        id: "11",
         "description": "Course Helicopter View",
         "duration": "08:19",
         "seqNo": 1,
-        courseId: 2
+        courseId: "2"
     },
 
     {
-        id: 12,
+        id: "12",
         "description": "Installing Git, Node, NPM and Choosing an IDE",
         "duration": "04:17",
         "seqNo": 2,
-        courseId: 2
+        courseId: "2"
     },
     {
-        id: 13,
+        id: "13",
         "description": "Installing The Lessons Code - Learn Why Its Essential To Use NPM 5",
         "duration": "06:05",
         "seqNo": 3,
-        courseId: 2
+        courseId: "2"
     },
     {
-        id: 14,
+        id: "14",
         "description": "How To Run Node In TypeScript With Hot Reloading",
         "duration": "03:57",
         "seqNo": 4,
-        courseId: 2
+        courseId: "2"
     },
     {
-        id: 15,
+        id: "15",
         "description": "Guided Tour Of The Sample Application",
         "duration": "06:00",
         "seqNo": 5,
-        courseId: 2
+        courseId: "2"
     },
     {
-        id: 16,
+        id: "16",
         "description": "Client Side Authentication Service - API Design",
         "duration": "04:53",
         "seqNo": 6,
-        courseId: 2
+        courseId: "2"
     },
     {
-        id: 17,
+        id: "17",
         "description": "Client Authentication Service - Design and Implementation",
         "duration": "09:14",
         "seqNo": 7,
-        courseId: 2
+        courseId: "2"
     },
     {
-        id: 18,
+        id: "18",
         "description": "The New Angular HTTP Client - Doing a POST Call To The Server",
         "duration": "06:08",
         "seqNo": 8,
-        courseId: 2
+        courseId: "2"
     },
     {
-        id: 19,
+        id: "19",
         "description": "User Sign Up Server-Side Implementation in Express",
         "duration": "08:50",
         "seqNo": 9,
-        courseId: 2
+        courseId: "2"
     },
     {
-        id: 20,
+        id: "20",
         "description": "Introduction To Cryptographic Hashes - A Running Demo",
         "duration": "05:46",
         "seqNo": 10,
-        courseId: 2
+        courseId: "2"
     },
     {
-        id: 21,
+        id: "21",
         "description": "Some Interesting Properties Of Hashing Functions - Validating Passwords",
         "duration": "06:31",
         "seqNo": 11,
-        courseId: 2
+        courseId: "2"
     },
 
 
     // PWA course
 
     {
-        id: 22,
+        id: "22",
         "description": "Course Kick-Off - Install Node, NPM, IDE And Service Workers Section Code",
         "duration": "07:19",
         "seqNo": 1,
-        courseId: 3
+        courseId: "3"
     },
     {
-        id: 23,
+        id: "23",
         "description": "Service Workers In a Nutshell - Service Worker Registration",
         "duration": "6:59",
         "seqNo": 2,
-        courseId: 3
+        courseId: "3"
     },
     {
-        id: 24,
+        id: "24",
         "description": "Service Workers Hello World - Lifecycle Part 1 and PWA Chrome Dev Tools",
         "duration": "7:28",
         "seqNo": 3,
-        courseId: 3
+        courseId: "3"
     },
     {
-        id: 25,
+        id: "25",
         "description": "Service Workers and Application Versioning - Install & Activate Lifecycle Phases",
         "duration": "10:17",
         "seqNo": 4,
-        courseId: 3
+        courseId: "3"
     },
 
     {
-        id: 26,
+        id: "26",
         "description": "Downloading The Offline Page - The Service Worker Installation Phase",
         "duration": "09:50",
         "seqNo": 5,
-        courseId: 3
+        courseId: "3"
     },
     {
-        id: 27,
+        id: "27",
         "description": "Introduction to the Cache Storage PWA API",
         "duration": "04:44",
         "seqNo": 6,
-        courseId: 3
+        courseId: "3"
     },
      {
-        id: 28,
+        id: "28",
         "description": "View Service Workers HTTP Interception Features In Action",
         "duration": "06:07",
         "seqNo": 7,
-        courseId: 3
+        courseId: "3"
     },
     {
-        id: 29,
+        id: "29",
         "description": "Service Workers Error Handling - Serving The Offline Page",
         "duration": "5:38",
         "seqNo": 8,
-        courseId: 3
+        courseId: "3"
     }
 
 ];
