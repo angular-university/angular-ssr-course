6d274bc0aa077fb99cc12e76eba03c8347101127
JHades <jhades.dev@gmail.com>
2018-02-05 12:04:19 +0100

    Angular Universal Course

--- server.ts
+++ server.ts
@@ -7,45 +7,26 @@ import { enableProdMode } from '@angular/core';
 import * as express from 'express';
 import { join } from 'path';
 import { readFileSync } from 'fs';
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
-// Faster server renders w/ Prod mode (dev mode never needed)
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist-server/main.bundle');
+
+
 enableProdMode();
 
-// Express server
 const app = express();
 
 const DIST_FOLDER = join(process.cwd(), 'dist');
 const DIST_SERVER_FOLDER = join(process.cwd(), 'dist-server');
 
-// Our index.html we'll use as our template
 const indexHtml = readFileSync(join(DIST_FOLDER, 'index.html'), 'utf-8').toString();
-
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist-server/main.bundle');
 
 
 /*
-// Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-    bootstrap: AppServerModuleNgFactory,
-    providers: [
-        provideModuleMap(LAZY_MODULE_MAP)
-    ]
-}));
-
-*/
-
-
+// simplified demo of how rendering is done on the server
 app.route('/').get((req, res) => {
-
-    console.log(indexHtml);
-
-    debugger;
 
     renderModuleFactory(AppServerModuleNgFactory, {
         document: indexHtml,
@@ -55,24 +36,29 @@ app.route('/').get((req, res) => {
 
 });
 
+*/
 
 
-// app.set('view engine', 'html');
-// app.set('views', DIST_FOLDER);
-
-
-/*
-// Server static files
-app.get('*.*', express.static(join(DIST_FOLDER, 'dist'), {
-    maxAge: '1y'
+app.engine('html', ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [
+        provideModuleMap(LAZY_MODULE_MAP)
+    ]
 }));
+
+app.set('view engine', 'html');
+app.set('views', DIST_FOLDER);
+
 
 // ALl regular routes use the Universal engine
 app.get('*', (req, res) => {
     res.render('index', { req });
 });
 
-*/
+// Server static files
+app.get('*.*', express.static(DIST_FOLDER, {
+    maxAge: '1y'
+}));
 
 
 // Start up the Node server
