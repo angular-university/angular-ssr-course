59aa4086cc71b6c683237d828b55f9c3ba535566
JHades <jhades.dev@gmail.com>
2018-02-05 14:11:29 +0100

    Angular Universal Course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
+    constructor(private titleService: Title, private metaService: Meta) {
+
+    }
+
+    ngOnInit() {
+
+        this.titleService.setTitle("BLAH!!");
+        this.metaService.addTag({name: "twitter:card", content: "BLAH"});
+
+    }
 
 }
