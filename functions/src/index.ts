bae5385c1b331c03e4149de6e56828e5952036c0
JHades <jhades.dev@gmail.com>
2018-02-06 10:45:24 +0100

    Angular Universal Course

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,12 @@
 
 import * as angularUniversal from 'angular-universal-express-firebase';
 
+const DIST_DIR =  __dirname + '/../../dist';
+
 export let ssrApp = angularUniversal.trigger({
-    staticDirectory: __dirname + '/browser',
-    index: __dirname + '/browser/index.html',
-    main: __dirname + '/server/main.bundle',
+    staticDirectory:  DIST_DIR + '/browser',
+    index: DIST_DIR + '/browser/index.html',
+    main: DIST_DIR + '/server/main.bundle',
     enableProdMode: true,
     cdnCacheExpiry: 600,
     browserCacheExpiry: 300,
