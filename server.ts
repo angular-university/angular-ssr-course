0d20bd831a8702c7b440aa5163b73c49bdc4ef52
Your Name <you@example.com>
2018-02-15 15:49:40 +0100

    Angular Universal Course

---
+++ server.ts
@@ -0,0 +1,42 @@
+
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
+import {renderModuleFactory} from '@angular/platform-server';
+import * as express from 'express';
+import { readFileSync } from 'fs';
+import { enableProdMode } from '@angular/core';
+
+const {AppServerModuleNgFactory} = require('./dist-server/main.bundle');
+
+enableProdMode();
+
+const app = express();
+
+const indexHtml = readFileSync(__dirname + '/dist/index.html', 'utf-8').toString();
+
+app.route('/').get((req, res) => {
+
+    renderModuleFactory(AppServerModuleNgFactory, {
+        document: indexHtml,
+        url: '/'
+    })
+        .then(html => res.status(200).send(html))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+
+});
+
+app.get('*.*', express.static(__dirname + '/dist', {
+    maxAge: '1y'
+}));
+
+app.listen(9000, () => {
+    console.log(`Angular Universal Node Express server listening on http://localhost:9000`);
+});
+
+
+
+
+
