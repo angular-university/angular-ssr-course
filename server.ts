26ac932de9a53fd5b3eb583ae877145b19b7829a
Your Name <you@example.com>
2018-02-15 16:02:02 +0100

    Angular Universal Course

--- server.ts
+++ server.ts
@@ -14,11 +14,16 @@ const app = express();
 
 const indexHtml = readFileSync(__dirname + '/dist/index.html', 'utf-8').toString();
 
-app.route('/').get((req, res) => {
+
+app.get('*.*', express.static(__dirname + '/dist', {
+    maxAge: '1y'
+}));
+
+app.route('*').get((req, res) => {
 
     renderModuleFactory(AppServerModuleNgFactory, {
         document: indexHtml,
-        url: '/'
+        url: req.url
     })
         .then(html => res.status(200).send(html))
         .catch(err => {
@@ -28,9 +33,7 @@ app.route('/').get((req, res) => {
 
 });
 
-app.get('*.*', express.static(__dirname + '/dist', {
-    maxAge: '1y'
-}));
+
 
 app.listen(9000, () => {
     console.log(`Angular Universal Node Express server listening on http://localhost:9000`);
