01e09658d51d19d5dc01ecbbe2ab78c45f84076b
Your Name <you@example.com>
2020-09-04 13:07:12 +0200

    Angular Universal In Depth

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import {Course} from "../model/course";
 import {Observable} from "rxjs";
 import {CoursesService} from "../services/courses.service";
 import {map, tap} from 'rxjs/operators';
+import {Title} from "@angular/platform-browser";
 
 @Component({
     selector: 'home',
@@ -13,11 +14,15 @@ export class HomeComponent implements OnInit {
 
     courses$: Observable<Course[]>;
 
-    constructor(private coursesService: CoursesService) {
+    constructor(private coursesService: CoursesService,
+                private title: Title) {
 
     }
 
     ngOnInit() {
+
+        this.title.setTitle("Angular University - All Courses");
+
         this.courses$ = this.coursesService.findAllCourses()
             .pipe(
                 map(Object.values)
